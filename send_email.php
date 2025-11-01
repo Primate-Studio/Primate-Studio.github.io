437a522caed274612b98efc890274289660d64e1
Adri1714 <adria.roger.juanola@gmail.com>
2025-11-01 20:42:23 +0100

    env

--- send_email.php
+++ send_email.php
@@ -3,6 +3,14 @@ require 'vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
+if (file_exists(__DIR__ . '/.env')) {
+    $lines = file(__DIR__ . '/.env', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (strpos(trim($line), '#') === 0) continue;
+        [$name, $value] = array_map('trim', explode('=', $line, 2));
+        putenv("$name=$value");
+    }
+}
 header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
